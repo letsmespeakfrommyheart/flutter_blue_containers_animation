d8f79021afcc6652d63ab10aa1b4203f2528848e
letsmespeakfrommyheart <maria@MacBook-Air-Vladimir.local>
2022-06-15 01:31:49 +0300

    create animation with colored containers

---
+++ android/app/src/main/kotlin/com/example/flutter_animation_blue_containers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_animation_blue_containers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blue Containers Animation'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.4, 0.6)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.yellow,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.60, 0.8)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.green,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.8, 1.0)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.2, 0.4)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.orange,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.4, 0.6)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.yellow,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.6, 0.8)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller,
+                      curve: const Interval(
+                        0.0,
+                        0.3,
+                      ),
+                      reverseCurve: const Interval(0.0, 0.2)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.red,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.2, 0.4)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.orange,
+                  ),
+                ),
+                FadeTransition(
+                  opacity: CurvedAnimation(
+                      parent: _controller, curve: const Interval(0.4, 0.6)),
+                  child: Container(
+                    height: 70,
+                    width: 70,
+                    color: Colors.yellow,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
